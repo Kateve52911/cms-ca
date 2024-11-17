57aa09c83186687ba9a59c1cf643eaeb1ba2131e
Kateve52911 <kateve52911@stud.noroff.no>
2024-11-17 11:31:30 +0100

    added constants file.

---
+++ js/constants.js
@@ -0,0 +1,3 @@
+export const API_URL = "https://www.kateve52911.com/wp-json/wc/v3/products";
+export const API_KEY = "ck_fcc90ac050e1868738b6d1f7eddbcac391482af2";
+export const API_SECRET = "cs_ab4df592fc719bc93fe365bc36232b569f3c5919";
